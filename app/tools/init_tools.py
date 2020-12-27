f9c0f2cb36a52753640a70016d334c85ecc320d8
chunsi yang <chunsi.yang@gmail.com>
2020-12-27 23:06:49 +1100

    fix bugs when init plugin requirements

--- app/tools/init_tools.py
+++ app/tools/init_tools.py
@@ -37,12 +37,15 @@ def install_app_require():
     for requirement in requirements:
         install_package(requirement["name"], requirement["version"])
 
+
 def install_plugin_require():
     """
         use pip to install packages stated in plugins' requirements.json
     :return:
     """
     plugins_name = get_all_plugin_name()
+    if plugins_name == '':
+        return
     for plugin in plugins_name.split(','):
         config = get_config('app/plugins/%s/requirements.json' % plugin)
         if config:
@@ -84,7 +87,7 @@ def init_app():
         init app
     :return:
     """
-    #install_app_require()
+    install_app_require()
     init_database()
     init_data()
     install_plugin_require()
